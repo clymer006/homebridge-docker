04bfc8d231f9b31c86118583acb5d5676a14f7a5
clymer006 <103019242+clymer006@users.noreply.github.com>
2023-08-22 14:04:09 -0500

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -71,7 +71,7 @@ echo " "
 echo "----------------------------------------------------------------"
 echo "Commence Watch Tower Setup"
 echo "----------------------------------------------------------------"
-sudo docker run --name="watchtower" -d --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+#sudo docker run --name="watchtower" -d --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
 echo "----------------------------------------------------------------"
 echo "Watch Tower Setup Completed"
 echo "----------------------------------------------------------------"
@@ -83,7 +83,7 @@ echo "----------------------------------------------------------------"
 echo "Commence MQTT Setup"
 echo "----------------------------------------------------------------"
 sudo wget https://raw.githubusercontent.com/EddieDSuza/maxilife/main/mosquitto.conf -P /home/$USER/docker/mosquitto/config/
-sudo docker run -it --name MQTT --restart unless-stopped --net=host -tid -p 1883:1883 -v $(pwd)/mosquitto:/mosquitto/ eclipse-mosquitto
+#sudo docker run -it --name MQTT --restart unless-stopped --net=host -tid -p 1883:1883 -v $(pwd)/mosquitto:/mosquitto/ eclipse-mosquitto
 echo "----------------------------------------------------------------"
 echo "MQTT Setup Completed"
 echo "----------------------------------------------------------------"
@@ -96,7 +96,7 @@ echo "Commence Zigbee2MQTT Setup"
 echo "----------------------------------------------------------------"
 wget https://raw.githubusercontent.com/EddieDSuza/techwitheddie/main/configuration.yaml -P data
 echo " "
-sudo docker run --name zigbee2mqtt --device=/dev/ttyACM0 --net host --restart unless-stopped -v $(pwd)/data:/app/data -v /run/udev:/run/udev:ro -e TZ=Asia/Dubai koenkk/zigbee2mqtt
+#sudo docker run --name zigbee2mqtt --device=/dev/ttyACM0 --net host --restart unless-stopped -v $(pwd)/data:/app/data -v /run/udev:/run/udev:ro -e TZ=Asia/Dubai koenkk/zigbee2mqtt
 echo "----------------------------------------------------------------"
 echo "Z2M Interface is reachable at homebridge.local:8081"
 echo "----------------------------------------------------------------"
@@ -107,7 +107,7 @@ echo " "
 echo "----------------------------------------------------------------"
 echo "Commence Scrypted Setup"
 echo "----------------------------------------------------------------"
-sudo docker run --name="scrypted" --network host -d --restart unless-stopped -v ~/.scrypted/volume:/server/volume koush/scrypted
+#sudo docker run --name="scrypted" --network host -d --restart unless-stopped -v ~/.scrypted/volume:/server/volume koush/scrypted
 echo "----------------------------------------------------------------"
 echo "Scrypted Interface is reachable at homebridge.local:10443 or port 11080"
 echo "----------------------------------------------------------------"
@@ -120,7 +120,7 @@ echo "Commence HEIMDALL Setup"
 echo "----------------------------------------------------------------"
 #mkdir /home/kodestar/docker
 echo " "
-sudo docker run --name=heimdall -d --restart unless-stopped -v /home/kodestar/docker/heimdall:/config -e PGID=1000 -e PUID=1000 -p 8201:80 -p 8200:443 lscr.io/linuxserver/heimdall:latest
+#sudo docker run --name=heimdall -d --restart unless-stopped -v /home/kodestar/docker/heimdall:/config -e PGID=1000 -e PUID=1000 -p 8201:80 -p 8200:443 lscr.io/linuxserver/heimdall:latest
 echo " "
 echo "----------------------------------------------------------------"
 echo "HEIMDALL Interface is reachable at homebridge.local:8201"
